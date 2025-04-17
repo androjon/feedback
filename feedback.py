2f64879d534cd7bf0d4027b3910ec4ddee0306fc
androjon <jonfindahl@gmail.com>
2025-04-17 14:10:51 +0200

    Update feedback.py

--- feedback.py
+++ feedback.py
@@ -10,7 +10,7 @@ from st_files_connection import FilesConnection
 # Create connection object and retrieve file contents.
 # Specify input format is a csv and to cache the result for 600 seconds.
 conn = st.connection('gcs', type=FilesConnection)
-df = conn.read("streamlit-bucket/myfile.csv", input_format="csv", ttl=600)
+df = conn.read("androjons_bucket/myfile.csv", input_format="csv", ttl=600)
 
 # Print results.
 for row in df.itertuples():
